0ec8422a68a2c086f9928532caaa0feb5488ecee
David Weid <david.weid.2@gmail.com>
2018-12-06 14:59:58 -0500

    variables created

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// VARIABLES //
+
+// An empty array to store user guesses (keys pressed)
+var lettersGuessed = [];
+
+// User starts with 12 guesses
+var guessesRemaining = 12;
+
+// User starts with 0 wins
+var wins = 0;
+
+// Array to store the words
+var words = ["christmas", "snowman", "mistletoe", "eggnog", "cookies", "presents", "elves", "santa", "reindeer", "snowflakes"];
+var wordIndex = 0;
+var currentWord = words[wordIndex];
+
+console.log(words[wordIndex]); // Logs "christmas"
+
+// FUNCTIONS //
+
